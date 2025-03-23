5bc6c861e0a40e96224466db6e32f8c7100080d9
Sebastian Jaramillo <sebasjaramas@hotmail.com>
2025-03-23 03:33:19 -0700

    Se metio lo basico de la navbar, pero se tuvo un problema al centrar todo
    Los botones, los logos, etc, no se quieren centrar por alguna razon, los archivos de assets como css y los de scss no son el problema, se desconoce si sea por la incompatiblidad de vue y bootstrap o de un archivo que lo este haciendo o simplemente lo estoy haciendo mal.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,25 +1,74 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+  import { RouterLink, useRoute } from 'vue-router';
+  import logo from '../../img/LogoAurora.png';
+  import tec from '../../img/tec.png';
+
+  const isActiveLink = (routePath) => {
+    const route = useRoute();
+    return route.path === routePath;
+  }
+
 </script>
 
 <template>
+  
   <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="/">Your App Name</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <RouterLink to="/" class="nav-link">Home</RouterLink>
-          </li>
-          <!-- Add your navigation links here -->
-        </ul>
+    <row>
+      <div class="container col-12 justify-content-center">
+        <header class="container-fluid d-flex justify-content-center">
+          <img :src="tec" width="700" height="100">
+        </header>
       </div>
-    </div>
+      
+      <div class="container col-12 justify-content-center">
+
+        <a class="navbar-brand">
+          <img :src="logo" width="125" height="50">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
+
+          <ul class="navbar-nav">
+
+            <li class="nav-item">
+              <RouterLink to="/" class="nav-link">
+                INICIO
+              </RouterLink>
+            </li>
+
+            <li class="nav-item">
+              <RouterLink to="/" class="nav-link">
+                BIBLIOTECA
+              </RouterLink>
+            </li>
+            
+            <li class="nav-item">
+              <RouterLink to="/" class="nav-link">
+                EVENTOS
+              </RouterLink>
+            </li>
+
+            <li class="nav-item">
+              <RouterLink to="/" class="nav-link">
+                COMUNIDAD
+              </RouterLink>
+            </li>
+
+            <!-- Add your navigation links here -->
+          </ul>
+
+        </div>
+
+      </div>
+
+    </row>
   </nav>
+
 </template>
 
 <style scoped>

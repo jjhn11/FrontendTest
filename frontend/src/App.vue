62138a92178156a03f96be616590f1cab1ab1ae5
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-03-25 21:01:47 -0700

    Carrusel funcional, falta diseño pero la base está (#7)
    * Carrusel funcional
    
    Carrusel ya realizado, falta editar las "cards" para que se parezca más al prototipo, el CSS esta en el mismo archivo que el Carrusel.
    Cambiar las imagenes por las del tec
    
    * Carrusel 1
    
    Carrusel funcional, falta mejorar el diseño para que se parezca más al prototipo, cambiar imagenes, y el css esta en el mismo archivo que el carrusel
    
    ---------
    
    Co-authored-by: Feli-deli <a22490375@itmexicali.edu.mx>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
+import Carrusel from './components/Carrusel.vue';
 import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div id="app">
     <Navbar />
+    <Carrusel />
     <RouterView />
+
   </div>
 </template>
 

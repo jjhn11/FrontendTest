6fb88d59d92f50cae42b21e0815f9517482421aa
Sebastian Jaramillo <sebasjaramas@hotmail.com>
2025-03-25 22:30:01 -0700

    Se resolvio el problema del centrado y se avanzo con la navbar
    Ya se hizo una solida base de la navbar, muy parecida a la del diseño de figma, se acomodaron la mayoria de componentes, se les cambio la fuente y el color de letra, se añadio el nombre de la empresa, y algunas transiciones con hovers en varios componentes.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -7,69 +7,125 @@
     const route = useRoute();
     return route.path === routePath;
   }
-
 </script>
 
 <template>
   
-  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <row>
-      <div class="container col-12 justify-content-center">
-        <header class="container-fluid d-flex justify-content-center">
-          <img :src="tec" width="700" height="100">
-        </header>
-      </div>
+  <div class="container-fluid mt-5">
+    
+    
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top p-1" id="nav">
       
-      <div class="container col-12 justify-content-center">
 
-        <a class="navbar-brand">
-          <img :src="logo" width="125" height="50">
-        </a>
+      <div class="col-12 mt-2">
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <raw>
+          
+          <!-- CONTAINER EDU -->
 
-        <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
+          <div class="d-flex justify-content-center mb-2">
 
-          <ul class="navbar-nav">
+            <header>
+              <img :src="tec" width="550" height="75">
+            </header>
+          
+          </div>
 
-            <li class="nav-item">
-              <RouterLink to="/" class="nav-link">
-                INICIO
-              </RouterLink>
-            </li>
+          <!-- CONTAINER NAVBAR -->
 
-            <li class="nav-item">
-              <RouterLink to="/" class="nav-link">
-                BIBLIOTECA
-              </RouterLink>
-            </li>
+          <div class="container-fluid p-2">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+              
+              <a class="navbar-brand ms-5" id="aurlog">
+                <img :src="logo" width="115" height="45">
+                <span>AURORA</span>
+              </a>
+
+              <ul class="navbar-nav ms-auto border-top border-black border-1 fs-5" id="navbton">
+
+                <li class="nav-item pe-5 mt-3 ms-3 me-5">
+                  <RouterLink to="/" class="nav-link" style="text-">
+                    INICIO
+                  </RouterLink>
+                </li>
+
+                <li class="nav-item pe-5 mt-3 ms-5 me-5">
+                  <RouterLink to="/" class="nav-link">
+                    BIBLIOTECA
+                  </RouterLink>
+                </li>
+                
+                <li class="nav-item pe-5 mt-3 ms-5 me-5">
+                  <RouterLink to="/" class="nav-link">
+                    EVENTOS
+                  </RouterLink>
+                </li>
+
+                <li class="nav-item mt-3 ms-5 me-2">
+                  <RouterLink to="/" class="nav-link">
+                    COMUNIDAD
+                  </RouterLink>
+                </li>
+
+                <!-- Add your navigation links here -->
+              </ul>
+
+              <button type="button" class="btn btn-primary mx-2 ms-auto mt-3"> 
+                <span class="fw-bold">Iniciar Sesion</span> 
+              </button>
+              
+              <button type="button" class="btn btn-outline-primary mt-3"> 
+                <span class="fw-bold">Registrarse</span> 
+              </button>
+
+            </div>
             
-            <li class="nav-item">
-              <RouterLink to="/" class="nav-link">
-                EVENTOS
-              </RouterLink>
-            </li>
 
-            <li class="nav-item">
-              <RouterLink to="/" class="nav-link">
-                COMUNIDAD
-              </RouterLink>
-            </li>
-
-            <!-- Add your navigation links here -->
-          </ul>
-
-        </div>
+          </div>
+    
+        </raw>
 
       </div>
 
-    </row>
-  </nav>
+    </nav>
+
+  </div>
 
 </template>
 
-<style scoped>
+<style>
+
+#nav li > a {
+  font-family: 'Anek Odia';
+  font-weight: 600;
+  height: 40px;
+  color: #000E32;
+  background-color: #FFFAE9;
+}
+
+#nav li > a:hover {
+    transition: 0.4s;
+    color: white;
+    background-color: #000E32;
+    border-radius: 5px;
+}
+
+#aurlog span {
+  font-family: 'Cinzel Decorative';
+  font-weight: 600;
+  color: #000E32;
+  opacity: 0;
+  transition: 0.5s ;
+}
+
+#aurlog span:hover {
+  opacity: 1;
+}
+
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 import store from './store'
 
 // Import Bootstrap CSS and JS
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
